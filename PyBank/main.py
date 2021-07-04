c670c7bf9a36bc24e43bb7af355de418c17d4e0f
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-07-04 09:16:04 -0500

    updated pybank

---
+++ PyBank/main.py
@@ -0,0 +1,42 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+raw_data = []
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    most_increase = 0
+    most_decrease = 0
+    m_ave = 0
+    csv_first = next(csvreader)
+    last_month = int(csv_first[1])
+    total_amount = last_month
+
+    for row in csvreader:
+        raw_data.append(row)
+        total_amount = total_amount + int(row[1])
+
+        average = int(row[1]) - last_month
+        m_ave = m_ave + average
+
+        if average > most_increase:
+            most_increase = average
+            increase_month = row[0]
+        elif average <  most_decrease:
+            most_decrease = average
+            decrease_month = row[0]
+        last_month = int(row[1])
+
+total_months = int(len(raw_data))
+
+print("Financial Analysis")
+print("--------------------------")
+print(f"Total Months: {total_months + 1}")
+print(f"Total: {total_amount}")
+print(f"Average Change: {m_ave/total_months}")
+print(f"Greatest Increase in Profits: {increase_month} ({most_increase})")
+print(f"Greatest Decrease in Profits: {decrease_month} ({most_decrease})")
