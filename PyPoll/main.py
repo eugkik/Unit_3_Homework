5c5e684640d6fb16fb2d86003236c0991b7e3dbe
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-07-14 07:40:35 -0500

    updated comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,10 @@
 import os
 import csv
 
-#source_data = os.path.join('Resources', 'Book1.csv')
+#define source path for csv file
 source_data = os.path.join('Resources', 'election_data.csv')
 
-#list to contain only candidate names from every row in csv file
+#list to contain candidate names from every row in csv file
 all_candidates = []
 
 #list to contain unique candidate names
@@ -17,9 +17,9 @@ totals = []
 with open(source_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     next(csvreader)
-  
+
 #loop through all rows and append candidate name into all_candidates list
-#if candidate name is not unique_candidate list, append into list
+#if candidate name is not in unique_candidate list, append into list
     for row in csvreader:
         all_candidates.append(row[2])
         if (row[2]) not in unique_candidates:
@@ -27,12 +27,12 @@ with open(source_data) as csvfile:
 
 #loop through each name in unique_candidates list
 #count name occurances in all_candidates list to determine total votes for name
-#append vote count and name into totals list
+#append list of vote count and name into totals list
 for names in unique_candidates:
     counts = all_candidates.count(names)
     totals.append([counts,names])    
 
-#find total votes cast from lenght of all_candidates list
+#find total votes cast in election from length of all_candidates list
 total_votes = len(all_candidates)
 
 #reverse sort totals list to order candidates from highest to lowest votes
@@ -45,12 +45,12 @@ print(f'Total Votes: {total_votes}')
 print('---------------------------------')
 
 #loop through number of unique candidates
-#for each candidate, calculate percent of votes received
+#calculate percent of votes received for each unique candidate
 #print formatted output to terminal
 for x in range(len(totals)):
    percent_votes = totals[x][0]/total_votes*100
    print(f"{totals[x][1]}: {format(percent_votes, '.3f')}% ({totals[x][0]})")
- 
+
 #print element 0 from sorted totals list as winner to terminal
 print('---------------------------------')
 print(f'Winner: {totals[0][1]}')
