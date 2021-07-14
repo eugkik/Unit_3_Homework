bfd693f0ef83fdcee94e26e97f0c726927e2bc0f
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-07-14 12:11:41 -0500

    updated comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,46 +13,66 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
 #initialize variables for greatest increase and decrease in profits
-    greatest_increase = 0
-    greatest_decrease = 0
+    greatest_increase_amount = 0
+    greatest_decrease_amount = 0
 
-#initialize variable for
-    m_ave = 0
+#initialize variable for total monthly profit/loss
+    monthly_total = 0
+
+#read first row of profit data
+#set initial values for calculating monthly average change
     csv_first = next(csvreader)
     last_month = int(csv_first[1])
-    total_amount = last_month
 
+#initialize variable for cumulative profit totals
+    total_amount = int(csv_first[1])
+
+#loop through all remaining rows and append date and profit data into list
+#keep running total of profits
     for row in csvreader:
         raw_data.append(row)
         total_amount = total_amount + int(row[1])
 
-        average = int(row[1]) - last_month
-        m_ave = m_ave + average
+#calculate monthly profit/loss
+        monthly_change = int(row[1]) - last_month
 
-        if average > greatest_increase:
-            greatest_increase = average
-            increase_month = row[0]
-        elif average <  greatest_decrease:
-            greatest_decrease = average
+#calculate cumulative total monthly profit/loss
+        monthly_total = monthly_total + monthly_change
+
+#compare current month's profit with greatest profit variable
+#record current month and amount if greater
+        if monthly_change > greatest_increase_amount:
+            greatest_increase_amount = monthly_change
+            greatest_increase_month = row[0]
+
+#compare current month's loss with greates loss variable
+#record current month and amount if less
+        elif monthly_change <  greatest_decrease_amount:
+            greatest_decrease_amount = monthly_change
             decrease_month = row[0]
+
+#set variable for next iteration
         last_month = int(row[1])
 
+#find total months from length of list
 total_months = int(len(raw_data))
 
+#print output to terminal
 print("Financial Analysis")
 print("--------------------------")
 print(f"Total Months: {total_months + 1}")
 print(f"Total: {total_amount}")
-print(f"Average Change: {m_ave/total_months}")
-print(f"Greatest Increase in Profits: {increase_month} ({greatest_increase})")
-print(f"Greatest Decrease in Profits: {decrease_month} ({greatest_decrease})")
+print(f"Average Change: {monthly_total/total_months}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} ({greatest_increase_amount})")
+print(f"Greatest Decrease in Profits: {decrease_month} ({greatest_decrease_amount})")
 
+#print output to text file
 output_path = os.path.join('Analysis', 'PyBank_Results.txt')
 with open(output_path, 'w') as text_out:
     print("Financial Analysis", file = text_out)
     print("--------------------------", file = text_out)
     print(f"Total Months: {total_months + 1}", file = text_out)
     print(f"Total: {total_amount}", file = text_out)
-    print(f"Average Change: {m_ave/total_months}", file = text_out)
-    print(f"Greatest Increase in Profits: {increase_month} ({greatest_increase})", file = text_out)
-    print(f"Greatest Decrease in Profits: {decrease_month} ({greatest_decrease})", file = text_out)
+    print(f"Average Change: {monthly_total/total_months}", file = text_out)
+    print(f"Greatest Increase in Profits: {greatest_increase_month} ({greatest_increase_amount})", file = text_out)
+    print(f"Greatest Decrease in Profits: {decrease_month} ({greatest_decrease_amount})", file = text_out)
